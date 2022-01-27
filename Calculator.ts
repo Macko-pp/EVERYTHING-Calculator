34b9e7de392ed563a398976a39153b4c8b175bd2
Macko-py <max.konietzko.maya@gmail.com>
2022-01-27 10:21:17 -0500

    Rounded result to 2 decimals

--- Calculator.ts
+++ Calculator.ts
@@ -8,116 +8,139 @@ function calc(var1, modifier, var2, var3) {
 	
 	if (modifier === "+") {
 		result = var1 + var2;
+		result = result.toFixed(2);
 		resultFormat = var1 + " + " + var2 + " = " + result;
 	}
 	
 	else if (modifier === "-") {
 		result = var1 - var2;
+		result = result.toFixed(2);
 		resultFormat = var1 + " - " + var2 + " = " + result;
 	}
 	
 	else if (modifier === "x" || modifier === "*") {
 		result = var1 * var2;
+		result = result.toFixed(2);
 		resultFormat = var1 + " x " + var2 + " = " + result;
 	}
 	
 	else if (modifier === "/" || modifier === "÷") {
 		result = var1 / var2;
+		result = result.toFixed(2);
 		resultFormat = var1 + " ÷ " + var2 + " = " + result;
 	}
 
 	else if (modifier === "^") {
 		result = Math.pow(var1, var2);
+		result = result.toFixed(2);
 		resultFormat = var1 + " ^ " + var2 + " = " + result;
 	}
 
 	else if (modifier === "%") {
 		result = (var1 / var2) * 100 + "%";
+		result = result.toFixed(2);
 		resultFormat = var1 + " % " + var2 + " = " + result;
 	}
 
 	else if (modifier === "√") {
 		result = Math.sqrt(var1);
+		result = result.toFixed(2);
 		resultFormat = "√" + var1 + " = " + result;
 	}
 
 	else if (modifier === "³√") {
 		result = Math.cbrt(var1);
+		result = result.toFixed(2);
 		resultFormat = "³√" + var1 + " = " + result;
 	}
 
 	else if (modifier === "△") {
 		result = (var1 * var2) / 2
+		result = result.toFixed(2);
 		resultFormat = var1 + " △ " + var2 + " = " + result;
 	}
 
 	else if (modifier === "◬") {
 		result = ((var1 * var2) / 2) * var3;
+		result = result.toFixed(2);
 		resultFormat = 'The Volume of ◬ is ' + result + "x³";
 	}
 
 	else if (modifier === "◬") {
 		result = ((var1 * var2) / 2) * var3;
+		result = result.toFixed(2);
 		resultFormat = 'The Volume of ❑ is ' + result + "x³";
 	}
 
 	else if (modifier === "❑") {
 		result = (var1 * var2) * var3;
+		result = result.toFixed(2);
 		resultFormat = 'The Volume of ❑ is ' + result + "x³";
 	}
 
 	else if (modifier === "⊿") {
 		result = Math.sqrt(Math.pow(var1, 2) + Math.pow(var2, 2));
+		result = result.toFixed(2);
 		resultFormat = "Hypotenuse of " + var1 + " and " + var2 + " = " + result;
 	}
 
 	else if (modifier === "Mi-Km") {
-		result = (var1 * 1.609) + "km";
+		result = var1 * 1.609;
+		result = result.toFixed(2);
 		resultFormat = var1 + "mi = " + result + "km";
 	}
 
 	else if (modifier === "Km-Mi") {
-		result = (var1 / 1.609) + "mi";
+		result = var1 / 1.609;
+		result = result.toFixed(2);
 		resultFormat = var1 + "km = " + result + "mi";
 	}
 
 	else if (modifier === "Ft-M") {
-		result = (var1 / 3.281) + "m";
+		result = var1 / 3.281;
+		result = result.toFixed(2);
 		resultFormat = var1 + "ft = " + result + "m";
 	}
 
 	else if (modifier === "M-Ft") {
-		result = (var1 * 3.281) + "ft";
+		result = var1 * 3.281;
+		result = result.toFixed(2);
 		resultFormat = var1 + "m = " + result + "ft";
 	}
 
 	else if (modifier === "Cm-In") {
-		result = (var1 / 2.54) + "in";
+		result = var1 / 2.54;
+		result = result.toFixed(2);
 		resultFormat = var1 + "cm = " + result + "in";
 	}
 
 	else if (modifier === "In-Cm") {
-		result = (var1 * 2.54) + "cm";
+		result = var1 * 2.54;
+		result = result.toFixed(2);
 		resultFormat = var1 + "in = " + result + "cm";
 	}
 
 	else if (modifier === "F-C") {
-		result = ((var1 - 32) * 5 / 9) + "°C";
+		result = (var1 - 32) * 5 / 9;
+		result = result.toFixed(2);
 		resultFormat = var1 + "°F = " + result + "°C";
 	}
 
 	else if (modifier === "C-F") {
-		result = ((var1 * 9 / 5) + 32) + "°F";
+		result = (var1 * 9 / 5) + 32;
+		result = result.toFixed(2);
 		resultFormat = var1 + "°C = " + result + "°F";
 	}
 
 	else if (modifier === "Kg-Lb") {
-		result = (var1 / 0.45359237) + "lb";
+		result = var1 / 0.45359237;
+		result = result.toFixed(2);
 		resultFormat = var1 + "kg = " + result + "lb";
 	}
 
 	else if (modifier === "Lb-Kg") {
-		result = (var1 * 0.45359237) + "kg";
+		result = var1 * 0.45359237;
+		result = result.toFixed(2);
 		resultFormat = var1 + "lb = " + result + "kg";
 	}
 	return result;
